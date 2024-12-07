43e8060d11a16c8cf1b4da883389567b6a72ea96
Mohammed Anees <mohammed.anees@pal.tech>
2024-12-08 01:38:55 +0530

    front

---
+++ f.js
@@ -0,0 +1,12 @@
+let btn = document.createElement("button");
+btn.innerText = "btn from JS";
+
+let div = document.querySelector('p');
+
+div.after(btn);
+
+
+function myfunction(par){
+return par;
+}
+
